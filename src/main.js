c245a047f99e0ca5bd55fb66335efe51735184b9
liupeijun <liupeijun1988@163.com>
2019-11-14 15:56:58 +0800

    dwa

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue' // var Vue = require('vue')
 import App from './App.vue'
 import router from './router'
 // import store from './store'
-// aa
 Vue.config.productionTip = false
 
 new Vue({
